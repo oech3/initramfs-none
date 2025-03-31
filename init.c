19c0d94f273b5d2683be81a3fd2868f6b60130d5
oech3 <79379754+oech3@users.noreply.github.com>
2025-04-01 00:47:43 +0900

    Release /init in initramfs

--- init.c
+++ init.c
@@ -7,13 +7,14 @@
 #include <sys/types.h>
 #include <fcntl.h> 
 
-
 int main() {
 	mknod("/dev/sda2", S_IFBLK | 0600, makedev(8, 2));
 	mount("/dev/sda2","/mnt","btrfs",MS_NOATIME,"subvol=@");
-	//syscall(__NR_pivot_root, "/mnt", "/"); ?
-	//switch_root? umount initramfs?
-	chroot("/mnt");
-	chdir("/");
+	unlink("/init"); //Is there any simple and safe method?
+	//Is this already simple? syscall No for pivot_root seems unstable.
+	chdir("/mnt");
+	mount(".", "/", NULL, MS_MOVE, NULL);
+	chroot(".");
+	chdir(".");
 	execv("/sbin/init", (char *[]) {"/sbin/init", NULL});
 }
