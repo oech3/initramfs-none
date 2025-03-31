c8cbdb35939ab2d2e7406feadc87d0be4f85aa78
oech3 <79379754+oech3@users.noreply.github.com>
2025-03-31 20:57:25 +0900

    comment about pivot_root

--- init.c
+++ init.c
@@ -7,9 +7,12 @@
 #include <sys/types.h>
 #include <fcntl.h> 
 
+
 int main() {
 	mknod("/dev/sda2", S_IFBLK | 0600, makedev(8, 2));
-	mount("/dev/sda2","/mnt","btrfs",MS_NOATIME,(const void *)"subvol=@");
+	mount("/dev/sda2","/mnt","btrfs",MS_NOATIME,"subvol=@");
+	//syscall(__NR_pivot_root, "/mnt", "/"); ?
+	//switch_root? umount initramfs?
 	chroot("/mnt");
 	chdir("/");
 	execv("/sbin/init", (char *[]) {"/sbin/init", NULL});
